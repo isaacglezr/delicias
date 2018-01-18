edf675b050b101d182415398aa98d59c06bb9cb6
isaacglezr <igr@soluciones4g.com>
2018-01-18 13:58:01 -0600

    Add files via upload

---
+++ medicos/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Medicos Delicias',
+    
+    'summary': """
+        Medicos Externos Delicias """,
+    
+    'description': """
+        Datos de los Medicos que prestan servicios a Clinica Delicias """,
+    
+    'author': "GBA Tecnologías",
+    'website': "http://www.gba.com.mx",
+    
+    'category': 'hr',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        'view/medicos_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,    
+}
---
+++ medicos/model/medicos.py
@@ -0,0 +1,9 @@
+from odoo import api, fields, model
+
+class Medicos(models.Model):
+
+    _name="medicos.delicias"
+
+    x_DocName = fields.Char(string="Nombre")
+    x_Especialidad = fields.Char(string="Especialidad")
+    x_movil = fields.Char(string="Telefono")
